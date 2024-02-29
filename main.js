ee9ebceaf4e20d76ea28e2b11fd543d935126fd2
shahjarar <160387866+shahjarar@users.noreply.github.com>
2024-02-29 18:01:36 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+// Task 23
+var car = "subaru";
+console.log("is car == 'subaru'? predict true");
+console.log(car == 'subaru');
+console.log("is car != 'Honda city'? predict true");
+console.log();
+console.log("is car == 'SUBARU'? predict false");
+console.log('car == SUBARU');
+console.log("is car.lenght == 6? predict true");
+console.log('car.lenght ==6');
+console.log("is car.lenght != 6? predict true");
+console.log('car.lenght !=10');
+console.log("is 10 > 45 ? predict false");
+console.log(10 > 45);
+console.log("is 3 <= 2? predict false");
+console.log(3 <= 2);
+console.log("is 72>= 83 ? predict false");
+console.log(72 >= 83);
+console.log("is car =='subaru'&& car.lenght ==6? predict true");
+console.log("car == 'sudaru' && Car.lenght==6");
